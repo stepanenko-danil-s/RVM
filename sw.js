1294037ecefb6528ff2182e0a441db63431b0ba8
stepanenko-danil-s <stepanenko.danil.s@gmail.com>
2026-06-22 14:13:05 +0200

    rollback sw cache bust

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='rvm-cache-v1162';
+const C='rvm-cache-v9000';
 self.addEventListener('install',function(e){self.skipWaiting();});
 self.addEventListener('activate',function(e){
   e.waitUntil(
