3212ac5e4331961c2fbe5dfa4e417c8331e7792c
stepanenko-danil-s <stepanenko.danil.s@gmail.com>
2026-06-23 22:02:43 +0200

    v11.89 sw

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='rvm-cache-v1197';
+const C='rvm-cache-v1198';
 self.addEventListener('install',function(e){self.skipWaiting();});
 self.addEventListener('activate',function(e){
   e.waitUntil(
