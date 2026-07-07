86971f543fb509e7ca0602073ae2900f2dc7c16d
stepanenko-danil-s <stepanenko.danil.s@gmail.com>
2026-07-07 14:27:56 +0200

    bump cache for new shops coords

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='rvm-cache-v1264';
+const C='rvm-cache-v1265';
 self.addEventListener('install',function(e){self.skipWaiting();});
 self.addEventListener('activate',function(e){
   e.waitUntil(
