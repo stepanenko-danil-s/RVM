66ef728cfdbdf68a7231ee874c99d8a090986aaa
stepanenko-danil-s <stepanenko.danil.s@gmail.com>
2026-07-07 07:55:01 +0200

    v12.36 — force cache update for shops.js coords

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='rvm-cache-v1245';
+const C='rvm-cache-v1246';
 self.addEventListener('install',function(e){self.skipWaiting();});
 self.addEventListener('activate',function(e){
   e.waitUntil(
