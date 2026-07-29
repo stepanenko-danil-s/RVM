00b11e50aefde32f762fc8669a8dc3b6b1e4f5d3
stepanenko-danil-s <stepanenko.danil.s@gmail.com>
2026-07-29 08:58:13 +0200

    v13.21: bump cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='rvm-cache-v1320';
+const C='rvm-cache-v1321';
 self.addEventListener('install',function(e){self.skipWaiting();});
 self.addEventListener('activate',function(e){
   e.waitUntil(
